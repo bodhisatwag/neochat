fcd836e1368f53dd8aeb840735f6bf39fe8b9133
Bodhisatwa Ghosh <bodhisatwa.gosh@allthatsdigital.co.uk>
2013-09-02 11:21:13 +0530

    Code Indentation 4 and roomindex.ejs created

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-var express = require('express')
-  , app = express()
-  , http = require('http')
-  , server = http.createServer(app)
-  , routes = require('./routes')
-  , socket = require('./routes/socket.js')
-  , io = require('socket.io').listen(server);
-
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    routes = require('./routes'),
+    socket = require('./routes/socket.js'),
+    io = require('socket.io').listen(server);
 
 
 // Heroku config only
 if(process.env.PORT) {
-  io.configure(function () { 
-    io.set("transports", ["xhr-polling"]); 
-    io.set("polling duration", 10); 
-  });  
+    io.configure(function () {
+        io.set("transports", ["xhr-polling"]);
+        io.set("polling duration", 10);
+    });
 }
-
 
 // Configuration
 var config = require('./config')(app, express);
@@ -29,7 +27,6 @@ app.get('/partials/:name', routes.partials);
 app.get('*', routes.index);
 
 // Socket.io Communication
-
 io.sockets.on('connection', socket);
 
 // Start server
--- config.js
+++ config.js
@@ -1,27 +1,26 @@
 module.exports = function(app, express, mongoose){
 
-  var config = this;
+    var config = this;
 
-  // Configuration
-  app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.set('view options', {
-      layout: false
+    // Configuration
+    app.configure(function(){
+        app.set('views', __dirname + '/views');
+        app.set('view engine', 'jade');
+        app.set('view options', {
+            layout: false
+        });
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(express.static(__dirname + '/public'));
+        app.use(app.router);
     });
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.static(__dirname + '/public'));
-    app.use(app.router);
-  });
 
-  app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  });
+    app.configure('development', function(){
+        app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    });
 
-  app.configure('production', function(){
-    app.use(express.errorHandler());
-  });
-  return config;
-
+    app.configure('production', function(){
+        app.use(express.errorHandler());
+    });
+    return config;
 };
